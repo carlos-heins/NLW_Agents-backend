7bbcbbea2766a31e54319efac36beb167cd574a4
carlos-heins <carlozheins053@gmail.com>
2025-07-14 12:53:41 -0300

    first setup of the backend project - creation of endpoint rooms and configutation of drizzle, biome, seed, docker and more

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import { fastifyCors } from '@fastify/cors';
+import { fastify } from 'fastify';
+import {
+    serializerCompiler,
+    validatorCompiler,
+    type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+import { env } from './env.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.register(fastifyCors, {
+    origin: true, // Allow all origins
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get('/healthcheck', () => {
+    return { message: 'Hello,  world!' };
+});
+
+app.register(getRoomsRoute);
+
+app.listen({ port: env.PORT })
